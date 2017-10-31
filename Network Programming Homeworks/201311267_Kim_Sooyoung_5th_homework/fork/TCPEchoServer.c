cd56340f391e3b210c4dc6f35ece91a1bca51b22
noreebia <noreebia@gmail.com>
2017-11-01 04:21:41 +0900

    Update

---
+++ Network Programming Homeworks/201311267_Kim_Sooyoung_5th_homework/fork/TCPEchoServer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXPENDING 5
+
+void DieWithError(char *errorMessage);
+void HandleTCPClient(int clntSocket);
+
+int main(void){
+	int servSock;
+	int clntSock;
+	struct sockaddr_in echoServAddr;
+	struct sockaddr_in echoClntAddr;
+	unsigned short echoServPort = 1080;
+	unsigned int clntLen;
+
+	if((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithError("socket() failed");
+
+	if (setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+	    DieWithError("setsockopt(SO_REUSEADDR) failed");
+
+	memset(&echoServAddr, 0, sizeof(echoServAddr));
+	echoServAddr.sin_family = AF_INET;
+	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	echoServAddr.sin_port = htons(echoServPort);
+
+	if(bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+		DieWithError("bind() failed");
+
+	if(listen(servSock, MAXPENDING) < 0)
+		DieWithError("listen() failed");
+
+	unsigned int childProcCount = 0;
+	while(1){
+		clntLen = sizeof(echoClntAddr);
+	
+		if((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
+			DieWithError("accept() failed");
+
+		printf("Client IP : %s\n", inet_ntoa(echoClntAddr.sin_addr));
+		printf("Port : %hu\n", ntohs(echoClntAddr.sin_port));
+
+		pid_t processID = fork();
+		if(processID < 0){
+			DieWithError("fork() failed");
+		}
+		else if(processID == 0){
+			close(servSock);
+			HandleTCPClient(clntSock);
+			exit(0);
+		}
+		childProcCount++;
+
+		printf("Listening again.\n\n");
+	}
+}
