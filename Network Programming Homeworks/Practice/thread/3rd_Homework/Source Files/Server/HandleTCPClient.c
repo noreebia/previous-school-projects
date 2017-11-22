afebf0e4d080667f45be78f79bc7e8e68f1fde99
noreebia <noreebia@gmail.com>
2017-11-22 20:35:49 +0900

    Update

---
+++ Network Programming Homeworks/Practice/thread/3rd_Homework/Source Files/Server/HandleTCPClient.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define RCVBUFSIZE 32
+
+void DieWithError(char *errorMessage);
+
+void HandleTCPClient(int clntSocket){
+	char echoBuffer[RCVBUFSIZE];
+	char *connectionInitString = "hi";
+	int recvMsgSize;
+
+	/* Clear the echoBuffer */
+	memset(echoBuffer, 0, RCVBUFSIZE);
+	
+	/* Receive string from client */
+	if((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) <0)
+		DieWithError("recv() failed");
+	
+	/*Display received string*/
+	printf("msg<- %s\n",echoBuffer);
+
+	/* If client sent "hello", reply with "hi" */
+	if( strcmp(echoBuffer, "hello") == 0){
+		if(send(clntSocket, connectionInitString, strlen(connectionInitString), 0) < 0){
+			DieWithError("send() failed");
+		}
+		printf("msg-> %s\n", connectionInitString);
+	}	
+	
+	printf("Commencing echo chat.\n\n");
+
+	/* Clear the buffer that stores the string that will be received and sent back */
+	memset(echoBuffer, 0, RCVBUFSIZE);
+	
+	while( recvMsgSize > 0){
+		/* Receive string from client */
+		if((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) <0)
+			DieWithError("recv() failed");	
+		printf("msg<- %s\n", echoBuffer);
+
+		/* Send to client the same exact string*/
+		if(send(clntSocket, echoBuffer, recvMsgSize, 0) != recvMsgSize)
+			DieWithError("send() failed");
+		printf("msg-> %s\n\n", echoBuffer);
+
+		/* Clear the buffer */
+		memset(echoBuffer, 0, RCVBUFSIZE);
+	}
+	printf("Closing socket.\n");
+	close(clntSocket);
+}
---
+++ Network Programming Homeworks/Practice/thread/3rd_Homework/Source Files/TCPEchoServer.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXPENDING 5
+
+void DieWithError(char *errorMessage);
+void HandleTCPClient(int clntSocket);
+
+void main(){
+	int servSock;
+	int clntSock;
+	struct sockaddr_in echoServAddr;
+	struct sockaddr_in echoClntAddr;
+	unsigned short echoServPort = 1080;
+	unsigned int clntLen;
+
+	int chatSock;
+	int clntSock2;
+	struct sockaddr_in chatServAddr;
+	struct sockaddr_in chatClientAddr;
+	unsigned short chatServPort = 1081;
+	unsigned int clntLen2;
+	
+
+	if((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithError("socket() failed");
+
+	if (setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+	    DieWithError("setsockopt(SO_REUSEADDR) failed");
+	
+	memset(&echoServAddr, 0, sizeof(echoServAddr));
+	echoServAddr.sin_family = AF_INET;
+	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	echoServAddr.sin_port = htons(echoServPort);
+
+	if(bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+		DieWithError("bind() failed");
+
+	if(listen(servSock, MAXPENDING) < 0)
+		DieWithError("listen() failed");
+
+	//chat socket
+	if((chatSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		DieWithError("socket() failed");
+
+	if (setsockopt(chatSock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
+	    DieWithError("setsockopt(SO_REUSEADDR) failed");
+	
+	memset(&chatServAddr, 0, sizeof(chatServAddr));
+	chatServAddr.sin_family = AF_INET;
+	chatServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	chatServAddr.sin_port = htons(chatServPort);
+
+	if(bind(chatSock, (struct sockaddr *) &chatServAddr, sizeof(chatServAddr)) < 0)
+		DieWithError("bind() failed");
+
+
+	if(listen(chatServAddr, MAXPENDING) < 0)
+		DieWithError("listen() failed");
+
+
+	while(1){
+		clntLen = sizeof(echoClntAddr);
+	
+		if((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
+			DieWithError("accept() failed");
+		
+		clntLen2 = sizeof(chatClientAddr);
+		if((clntSock2 = accept(chatSock, (struct sockaddr *) &chatClientAddr, &clntLen2)) < 0)
+			DieWithError("accept() failed");
+
+		printf("Client IP : %s\n", inet_ntoa(echoClntAddr.sin_addr));
+		printf("Port : %hu\n", ntohs(echoClntAddr.sin_port));
+
+		HandleTCPClient(clntSock);
+		printf("Listening again.\n\n");
+	}
+}
